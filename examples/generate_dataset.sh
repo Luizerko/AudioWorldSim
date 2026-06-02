ab43bec47cfe9fa16c23c17f34feb9f0c4cc3fc3
Luizerko <luisvz@gmail.com>
2026-06-02 17:12:44 -0300

    Using xargs to control the amounf of jobs in parallel when generating the dataset and fixing a bug to make sure audio processing runs only after the simulation for the seed ends.

--- examples/generate_dataset.sh
+++ examples/generate_dataset.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for i in {1..10}
-do
-    python examples/custom_example.py --dataset_instance mp3d.17DRP5sb8fy --input_audio /mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d_example/sounds/alarm/alarm.wav --sample_rate 44100 --simulation rollout --navigation target --seed $i &
-
-    python examples/audio_processing.py --file /mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d_example/sounds/alarm/simulation/target/seed_$i/output.wav &
-done
+seq 1 10 | xargs -I {} -P 5 bash -c ' \
+  python examples/custom_example.py --dataset_instance mp3d.17DRP5sb8fy --input_audio /mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d_example/sounds/alarm/alarm.wav --sample_rate 44100 --simulation rollout --navigation target --seed "{}" && \
+  python examples/audio_processing.py --file "/mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d_example/sounds/alarm/simulation/target/seed_{}/output.wav" \
+'
