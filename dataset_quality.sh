5fcd9ad366240aec461855f0db2e7ae82b7409f8
Luizerko <luisvz@gmail.com>
2026-06-22 17:59:25 -0300

    Computing total audio time.

--- dataset_quality.sh
+++ dataset_quality.sh
@@ -6,6 +6,7 @@ OUTPUT_FILE="problematic_seeds.txt"
 total_seeds=0
 empty_seeds=0
 stuck_seeds=0
+total_actions=0
 > "$OUTPUT_FILE"
 
 # Looping all simulations
@@ -24,9 +25,18 @@ for dir in "$BASE_DIR"/seed_*/; do
     elif [ -f "${dir}log_stuck" ]; then
         stuck_seeds=$((stuck_seeds + 1))
         echo "$seed_name - stuck" >> "$OUTPUT_FILE"
+
+    # Processing total time for valid simulations
+    else
+        action_file="${dir}action_list.txt"
+        action_count=$(tr -cd ',' < "$action_file" | wc -c)
+        total_actions=$((total_actions + action_count))
     fi
 done
 
 echo "Total Navigations:      $total_seeds"
 echo "Broken Navigations:     $empty_seeds"
 echo "Stuck Navigations:      $stuck_seeds"
+
+total_audio_time=$(awk "BEGIN {print $total_actions*0.2}")
+echo "Total Audio Time (s):   $total_audio_time"
