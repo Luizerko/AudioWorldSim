3870ed21c19157d69339bab8fd0a9a622647d87f
Luizerko <luisvz@gmail.com>
2026-07-02 17:06:58 -0300

    Fixing path for dataset generation yet again.

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 seq 1 1000 | xargs -I {} -P 30 bash -c ' \
-  python examples/custom_simulator.py --dataset_instance mp3d.17DRP5sb8fy --input_audio /mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d/alarm/alarm.wav --simulation rollout --navigation target --seed "{}" --video && \
+  python custom_simulator/simulator.py --dataset_instance mp3d.17DRP5sb8fy --input_audio /mnt/f/lz/visgraf/soundspaces/data/mp3d/alarm/alarm.wav --simulation rollout --navigation target --seed "{}" --video && \
   { \
-    python custom_simulator/audio_processing.py --file "/mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d/alarm/17DRP5sb8fy/rollout/target/seed_{}/output.wav" --method mel & \
-    python custom_simulator/audio_processing.py --file "/mnt/c/Users/luisvz/Documents/visgraf/soundspaces/data/mp3d/alarm/17DRP5sb8fy/rollout/target/seed_{}/output.wav" --method stft & \
+    python custom_simulator/audio_processing.py --file "/mnt/f/lz/visgraf/soundspaces/data/mp3d/alarm/17DRP5sb8fy/rollout/target/seed_{}/output.wav" --method mel & \
+    python custom_simulator/audio_processing.py --file "/mnt/f/lz/visgraf/soundspaces/data/mp3d/alarm/17DRP5sb8fy/rollout/target/seed_{}/output.wav" --method stft & \
     wait;
   } \
